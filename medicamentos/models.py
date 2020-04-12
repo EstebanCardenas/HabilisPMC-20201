97979eb2330c18057032f39b82ec351fc7d3cead
Esteban Cárdenas <ne.cardenas@uniandes.edu.co>
2020-04-12 12:52:57 -0500

    pedido, usuario, paciente

--- medicamentos/models.py
+++ medicamentos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Pedido(models.Model):
+    #Atributos
+    fecha = models.DateField(auto_now_add=True)
+    direccion = models.CharField(max_length=100)
+    precioTotal = models.FloatField()
+    #Relaciones
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    #Atributos
+    cedula = models.BigIntegerField()
+    nombre = models.CharField(max_length=50)
+    celular = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    #Relaciones
+
+class Paciente(Usuario):
+    #Atributos
+    edad = models.IntegerField()
+    #Relaciones
