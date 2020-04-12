d2ec26e9a54c4783cfbb40c8767ff665a4b24361
Esteban Cárdenas <ne.cardenas@uniandes.edu.co>
2020-04-12 13:39:30 -0500

    relaciones moxi

--- medicamentos/models.py
+++ medicamentos/models.py
@@ -8,17 +8,21 @@ class Pedido(models.Model):
     direccion = models.CharField(max_length=100)
     precioTotal = models.FloatField()
     #Relaciones
+    paciente = models.ForeignKey('usuarios.Paciente', on_delete=models.CASCADE)
+    #Funciones
+    def __str__(self):
+        '%s, %s, %s, $s' % (self.fecha.__str__(), self.direccion, self.precioTotal.__str__(), self.paciente.nombre)
 
 class Medicamento(models.Model):
     #Atributos
-    referencia: models.CharField(max_length=120)
-    descripcion: models.CharField(max_length=200)
-    indicaciones: models.CharField(max_length=250)
-    precio: models.FloatField()
+    referencia = models.CharField(max_length=120)
+    descripcion = models.CharField(max_length=200)
+    indicaciones = models.CharField(max_length=250)
+    precio = models.FloatField()
     #Relaciones
 
 class MedicamentoPedido(models.Model):
     #Atributos
-    cantidad: models.SmallIntegerField()
-    precioTotal: models.FloatField()
+    cantidad = models.SmallIntegerField()
+    precioTotal = models.FloatField()
     #Relaciones
