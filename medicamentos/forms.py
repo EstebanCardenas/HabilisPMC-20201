1496b18760da5a86ace6755f6303aab4840024a2
Esteban Cárdenas <ne.cardenas@uniandes.edu.co>
2020-04-29 02:04:45 -0500

    ui paciente

--- medicamentos/forms.py
+++ medicamentos/forms.py
@@ -19,7 +19,11 @@ class PedidoForm(forms.ModelForm):
         self.paciente = pc
 
 class MedicamentoPedidoForm(forms.ModelForm):
-    cantidad = forms.IntegerField(label="Cantidad")
+    cantidad = forms.IntegerField(label="Cantidad", widget=forms.NumberInput(
+        attrs={
+            'placeholder':'Introduzca la cantidad'
+        }
+    ))
     medicamento = forms.ModelChoiceField(label="Medicamento", queryset=get_all_products())
     pedido = forms.ModelChoiceField(label="Pedido", queryset=get_pedidos())
     class Meta:
@@ -33,6 +37,6 @@ class MedicamentoPedidoForm(forms.ModelForm):
 class OrdenMedicaForm(forms.Form):
     numRegistro = forms.IntegerField(label='Número de Solicitud', widget=forms.NumberInput(
         attrs={
-            'placeholder':'Introduzca su número de solicitud'
+            'placeholder':'Introduzca su número de solicitud',
         }
     ))
